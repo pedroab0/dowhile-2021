53e9e5dec47eade661034c0ab46fb1e232c0ab33
Pedro Barbosa <p.brbsa@gmail.com>
2021-10-21 14:23:03 -0300

    bug fix

--- backend/src/middleware/ensureAuthenticated.ts
+++ backend/src/middleware/ensureAuthenticated.ts
@@ -14,7 +14,7 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
         });
     }
 
-    const [, token] = authToken.split("");
+    const [, token] = authToken.split(" ");
 
     try {
         const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
